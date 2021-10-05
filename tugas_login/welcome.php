2e163e8ff5fc7898029ba6d521b718bb0c7bef1d
avcna <87115452+avcna@users.noreply.github.com>
2021-10-05 11:34:32 +0700

    Update welcome.php

--- tugas_login/welcome.php
+++ tugas_login/welcome.php
@@ -1,9 +1,11 @@
 <?php
+include 'config.php';
 session_start();
 
 if (!isset($_SESSION['username'])) {
   header("Location: login.php");
 }
+
  ?>
 
 <!DOCTYPE html>
@@ -18,9 +20,43 @@ if (!isset($_SESSION['username'])) {
     <title></title>
   </head>
   <body>
-      <p>Welcome to the website!</p>
+      <p>Data Mahasiswa</p>
+      <div class="card mx-auto" style="width:70%;margin-top:5%;">
+        <div class="card-header bg-success text-white text-center">
+          DATA MAHASISWA
+        </div>
+      <div class="card-body">
+      <table class="table table-borderd table-striped">
+        <tr>
+          <th>No</th>
+          <th>Nama</th>
+          <th>Fakultas</th>
+          <th>Prodi</th>
+        </tr>
+        <?php
+$no=1;
+        $tampil= mysqli_query($koneksi, "SELECT * FROM data");
+          while ($data = mysqli_fetch_array($tampil)) :
+
+         ?>
+        <tr>
+          <td><?=$no++;?></td>
+          <td><?=$data['nama']?></td>
+          <td><?=$data['fakultas']?></td>
+          <td><?=$data['prodi']?></td>
+          <td>
+
+          </td>
+        </tr>
+      <?php endwhile; ?>
+      </table>
+      <br>
+      <br>
       <a href="logout.php">
       <button class="btn btn-danger" type="submit" name="but_logout">Logout</button>
       </a>
+    </div>
+    </div>
+
   </body>
 </html>
